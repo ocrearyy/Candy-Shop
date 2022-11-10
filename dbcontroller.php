e49c112e432b0073596ffd8cd83d0eeb3031533c
oshanedesign <oshane.design@gmail.com>
2022-11-10 18:56:25 -0500

    add php build and xampp config

--- dbcontroller.php
+++ dbcontroller.php
@@ -15,6 +15,18 @@ class DBController {
 		return $conn;
 	}
 
+	//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
 	function runQuery($query) {
 		$result = mysqli_query($this->conn,$query);
 		while($row=mysqli_fetch_assoc($result)) {
