d1060c81be324e87a0b4ba0a2816faa96ef494a6
oshanedesign <oshane.design@gmail.com>
2022-11-11 10:00:17 -0500

    tests config

--- dbcontroller.php
+++ dbcontroller.php
@@ -2,25 +2,6 @@
 class DBController {
 
 	
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
-
-// function __construct() {
-// 		$this->conn = $this->connectDB();
-// 	}
-	
-// 	function connectDB() {
-// 		$conn = new mysqli($server, $username, $password, $db);
-// 		return $conn;
-// 	}
-
-// $conn = new mysqli($server, $username, $password, $db);
-
 	// private $host = "localhost";
 	// private $user = "root";
 	// private $password = "test";
@@ -37,15 +18,15 @@ $conn = new mysqli($server, $username, $password, $db);
 	// }
 
 	//Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 	function runQuery($query) {
