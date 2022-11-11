90d64b94468517b7ffbf13402ec005d0a7bc8deb
oshanedesign <oshane.design@gmail.com>
2022-11-11 18:25:02 -0500

    push

--- dbcontroller.php
+++ dbcontroller.php
@@ -1,29 +1,21 @@
 <?php
 class DBController {
-//Get Heroku ClearDB connection information
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+	private $host = "localhost";
+	private $user = "root";
+	private $password = "test";
+	private $database = "blog_samples";
+	private $conn;
+	
+	function __construct() {
+		$this->conn = $this->connectDB();
+	}
 
-$conn = new mysqli($server, $username, $password, $db);
-	// private $host = "localhost";
-	// private $user = "root";
-	// private $password = "test";
-	// private $database = "blog_samples";
-	// private $conn;
-	///
-	// function __construct() {
-	// 	$this->conn = $this->connectDB();
-	// }
-	// ///
-	// function connectDB() {
-	// 	$conn = mysqli_connect("localhost", "root", "", "tblproduct");
-	// 	return $conn;
-	// }
-///
+	function connectDB() {
+		$conn = mysqli_connect("localhost", "root", "", "tblproduct");
+		return $conn;
+	}
+
 	function runQuery($query) {
 		$result = mysqli_query($this->conn,$query);
 		while($row=mysqli_fetch_assoc($result)) {
